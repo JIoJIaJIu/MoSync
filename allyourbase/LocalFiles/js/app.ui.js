6e2343d42cca1bf03d8cc9f6fd8a89eac371ecf0
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-05-18 15:37:35 +0400

    template project

--- allyourbase/LocalFiles/js/app.ui.js
+++ allyourbase/LocalFiles/js/app.ui.js
@@ -7,217 +7,169 @@
 * without having to update/rewrite all the UI code.
 */
 
-app.ui = (function()
+app.ui = (function() {
+    var ui = {};
+
+    // Make sure we get the correct event type on every touch system.
+
+    ui.showMessage = function(message)
+    {
+        // Windows Phone does not support alert, show the message
+        // in the user name field for now.
+        if (mosync.isWindowsPhone)
+        {
+            ui.setUserNameField(message);
+        }
+        else
+        {
+            alert(message);
+        }
+    };
+
+    ui.getUserNameField = function() {
+        return $("#userNameField").val();
+    };
+
+    ui.setUserNameField = function(name) {
+        return $("#userNameField").val(name);
+    };
+
+
+    /**
+        * Keydown event listener. Gets called when the user presses
+        * a button on his/her keyboard in the input field.
+        * @param keynum The pressed key (as an ASCII value).
+        */
+    ui.keydown = function(keynum)
+    {
+        if (keynum == 13)
+        {
+            // TODO: Perhaps we need to use a timer to
+            // fix a virtual keyboard bug on Android 4.
+            setTimeout(function () {
+                // TODO 
+            }, 1);
+        }
+    };
+
+    /**
+    * Set up event bindings and initialize the UI.
+    */
+    ui.initialize = function()
+    {
+        // Handle the back key event (on Android).
+        document.addEventListener(
+            "backbutton",
+            function()
+            {
+                ui.goBack();
+            },
+            true
+        );
+
+        ui.initializeFramework();
+
+        ui.readAndDisplayFavouriteUsers();
+    };
+
+
+    /**
+        * Displays a loading indicator in the UI.
+        */
+    ui.showLoadingIndicator = function()
+    {
+        alert("ui.showLoadingIndicator: Implement for the specific UI framework used");
+    };
+
+
+    /**
+        * Called by framework when document has loaded.
+        */
+    ui.initializeFramework = function()
+    {
+        alert("ui.initializeFramework: Implement for the specific UI framework used");
+    };
+
+    /**
+        * Navigate back one page.
+        */
+    ui.goBack = function()
+    {
+        alert("ui.goBack: Implement for the specific UI framework used");
+    };
+
+    /**
+        * Show one page.
+        */
+    ui.showPage = function(page)
+    {
+        alert("ui.showPage: Implement for the specific UI framework used");
+    };
+
+    return ui;
+    })();
+
+    // Call app.ui.initialize when document has loaded.
+    document.addEventListener(
+    "DOMContentLoaded",
+    app.ui.initialize,
+    false
+    );
+
+
+/**
+    * Here we add jQuery Mobile specific code.
+    */
+(function()
 {
-	var ui = {};
+    /**
+        * Show one page.
+        */
+    app.ui.showPage = function(page)
+    {
+        $.mobile.changePage("#" + page);//, {transition: "slideleft"});
+    };
 
-	// Make sure we get the correct event type on every touch system.
+    /**
+        * Navigate back one page.
+        */
+    app.ui.goBack = function()
+    {
+        //console.log("@@@ app.ui.goBack activePage: " + $.mobile.activePage[0].id);
 
-	ui.showMessage = function(message)
-	{
-		// Windows Phone does not support alert, show the message
-		// in the user name field for now.
-		if (mosync.isWindowsPhone)
-		{
-			ui.setUserNameField(message);
-		}
-		else
-		{
-			alert(message);
-		}
-	};
+        if ($.mobile.activePage[0].id === "home")
+        {
+            // Send the app to the background if
+            // we are on the home screen (first page).
+            //mosync.app.sendToBackground();
+            mosync.app.exit();
+        }
+        else
+        {
+            // Otherwise navigate to previous screen.
+            history.back();
+            return false;
+        }
+    };
 
-	ui.getUserNameField = function()
-	{
-		return $("#userNameField").val();
-	};
+    /**
+        * Called by framework when document has loaded.
+        */
+    app.ui.initializeFramework = function()
+    {
+        // No special initialization is performed.
+    };
 
-	ui.setUserNameField = function(name)
-	{
-		return $("#userNameField").val(name);
-	};
 
-	/**
-	 * Show the tweets for the user in the input form.
-	 */
-	ui.showTweets = function()
-	{
-		// Get the user name from the input field.
-		var user = app.ui.getUserNameField();
-		if (user.length === 0)
-		{
-			ui.showMessage("ui.showTweets: Invalid username");
-		}
-		else
-		{
-			ui.showTweetsForUser(user);
-		}
-	};
+    /**
+        * Displays a loading indicator in the UI.
+        */
+    app.ui.showLoadingIndicator = function()
+    {
+        // We do not set this due to problems with
+        // dynamically updating/refreshing the DOM/UI
+        // of jQuery Mobile apps.
+        // TODO: Use the loading support of jQuery Mobile.
+    };
 
-	/**
-	 * Outputs the received tweets to the user-interface.
-	 * @param tweets The tweets as an array. Each element
-	 * represents one tweet and can be accessed via tweets[i].text
-	 */
-	ui.showTweetsForUser = function(user)
-	{
-		// Go to second screen.
-		ui.showPage("tweetList");
-		ui.showTweetListUserName(user);
-		ui.showLoadingIndicator();
-
-		// Get tweets.
-		app.getTweetsForUser(user, function(tweets)
-		{
-			ui.showTweetList(tweets);
-		});
-	};
-
-	/**
-	 * Read and display list of favourite users.
-	 */
-	ui.readAndDisplayFavouriteUsers = function()
-	{
-		app.readFavouriteUsers(function(userData, path)
-		{
-			if (userData)
-			{
-				ui.displayFavouriteUsers(userData);
-			}
-		});
-	};
-
-	/**
-	 * Add a user to the favourite list.
-	 */
-	ui.addFavouriteUser = function()
-	{
-		// Get the username from the input field
-		var userName = app.ui.getUserNameField();
-		if (userName.length === 0)
-		{
-			ui.showMessage("Invalid username")
-			ui.showPage("home");
-			return;
-		}
-
-		app.addFavouriteUser(userName, function(userList)
-		{
-			if (!userList)
-			{
-				app.ui.showMessage("Could not save user");
-			}
-			else
-			{
-				ui.displayFavouriteUsers(userList);
-				ui.showMessage("User successfully saved");
-			}
-		});
-	};
-
-	/**
-	 * Keydown event listener. Gets called when the user presses
-	 * a button on his/her keyboard in the input field.
-	 * @param keynum The pressed key (as an ASCII value).
-	 */
-	ui.keydown = function(keynum)
-	{
-		if (keynum == 13)
-		{
-			// TODO: Perhaps we need to use a timer to
-			// fix a virtual keyboard bug on Android 4.
-			//setTimeout(function () {
-				ui.showTweets();
-			//}, 1);
-		}
-	};
-
-	/**
-	 * Set up event bindings and initialize the UI.
-	 */
-	ui.initialize = function()
-	{
-		// Handle the back key event (on Android).
-		document.addEventListener(
-			"backbutton",
-			function()
-			{
-				ui.goBack();
-			},
-			true);
-
-		ui.initializeFramework();
-
-		ui.readAndDisplayFavouriteUsers();
-	};
-
-	/**
-	 * Diplay the list of favourite users in the UI.
-	 * @param userData Array of user names.
-	 */
-	ui.displayFavouriteUsers = function(userData)
-	{
-		alert("ui.displayFavouriteUsers: Implement for the specific UI framework used");
-	};
-
-	/**
-	 * Displays the Twitter user name in the UI.
-	 * @param user The Twitter user name.
-	 */
-	ui.showTweetListUserName = function(user)
-	{
-		//$("#pageTitle").html(user);
-		alert("ui.showTweetListUserName: Implement for the specific UI framework used");
-	};
-
-	/**
-	 * Displays a loading indicator in the UI.
-	 */
-	ui.showLoadingIndicator = function()
-	{
-		//$("#tweetList").html("<li>Loading...</li>");
-		alert("ui.showLoadingIndicator: Implement for the specific UI framework used");
-	};
-
-	/**
-	 * Outputs the list of tweets to the user-interface.
-	 * @param tweets The tweets as an array. Each element
-	 * represents one tweet and can be accessed via tweets[i].text
-	 */
-	ui.showTweetList = function(tweets)
-	{
-		alert("ui.showTweetList: Implement for the specific UI framework used");
-	};
-
-	/**
-	 * Called by framework when document has loaded.
-	 */
-	ui.initializeFramework = function()
-	{
-		alert("ui.initializeFramework: Implement for the specific UI framework used");
-	};
-
-	/**
-	 * Navigate back one page.
-	 */
-	ui.goBack = function()
-	{
-		alert("ui.goBack: Implement for the specific UI framework used");
-	};
-
-	/**
-	 * Show one page.
-	 */
-	ui.showPage = function(page)
-	{
-		alert("ui.showPage: Implement for the specific UI framework used");
-	};
-
-	return ui;
 })();
-
-// Call app.ui.initialize when document has loaded.
-document.addEventListener(
-	"DOMContentLoaded",
-	app.ui.initialize,
-	false);
