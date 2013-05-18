48949d0a9073fd40e42279ebc56d14a3986e5f4f
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-05-18 16:44:26 +0400

    вынес js в app.js

---
+++ fastdie/LocalFiles/js/app.js
@@ -0,0 +1,57 @@
+
+// Register event listeners.
+
+// The "deviceready" event is sent when the system
+// has finished loading.
+document.addEventListener(
+    "deviceready",
+    initialize,
+    true);
+
+
+function initialize () {
+    // Close the application when the back key is pressed
+    document.addEventListener(
+        "backbutton",
+        function() { mosync.app.exit(); },
+        true
+    );
+    document.getElementById('find-device').addEventListener('click', function () {
+        alert('123');
+    });
+}
+
+//function changeColor()
+//{
+    //mosync.nativeui.callJS(
+        //mosync.nativeui.MAIN_WEBVIEW,
+        //"performChangeColor()");
+//}
+
+    //Change page background to a random color.
+//function performChangeColor()
+//{
+    //var color = "#" +
+        //(Math.random() * 0xFFFFFF + 0x1000000)
+            //.toString(16).substr(1,6);
+    //document.documentElement.style.backgroundColor = color;
+    //document.body.style.backgroundColor = color;
+//}
+
+/**
+* Vibrate device.
+*/
+function vibrate()
+{
+    mosync.bridge.send(["Custom", "Vibrate", "500"]);
+}
+
+/**
+* Play one beep sound.
+*/
+function beep()
+{
+    // Send message to C++ to make device beep.
+    // Here we used the string stream format.
+    mosync.bridge.send(["Custom", "Beep"]);
+}
