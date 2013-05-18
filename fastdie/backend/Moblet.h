b3872ac87ab7b889868553cf53c31fae062952e0
Guro Bokum <jiojiajiu@yandex-team.ru>
2013-05-19 03:06:07 +0400

    refactoring

---
+++ fastdie/backend/Moblet.h
@@ -0,0 +1,21 @@
+#include <Wormhole/HybridMoblet.h>
+#include <conprint.h>
+
+#include "Discoverer.h"
+#include "BluetoothListeners.h"
+#include "utils/Logger.h"
+
+using namespace NativeUI;
+
+class MyMoblet : public Wormhole::HybridMoblet
+{
+public:
+	MyMoblet();
+    void vibrate(Wormhole::MessageStream& message);
+    void beep(Wormhole::MessageStream& message);
+    void findDevices(Wormhole::MessageStream& messsage);
+    void log(Wormhole::MessageStream& message);
+private:
+    BluetoothDiscoverer *mDiscoverer;
+    Logger *mLogger;
+};
