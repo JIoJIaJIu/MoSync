455d406d6ae6d787632100cfb5c206a68bf730b7
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-05-18 23:39:20 +0400

    logger

--- fastdie/LocalFiles/js/ui.js
+++ fastdie/LocalFiles/js/ui.js
@@ -1,20 +1,31 @@
-function initialize () {
-    // Close the application when the back key is pressed
-    document.addEventListener(
-        "backbutton",
-        function() { mosync.app.exit(); },
-        true
-    );
-
-
+function onSuccess () {
+    $('body').html(JSON.stringify(arguments));
 }
+
+function onError (argument) {
+    $('body').html(JSON.stringify(arguments));
+}
+
 $(function () {
+
     $('.find-device').on('click', function () {
         $('.main-page').hide();
         $('.devices').show();
         sendToPlatform(['findDevices'], function () {
             $('body').html(JSON.stringify(arguments));
             alert(JSON.stringify(arguments));
-        })
+        });
     });
+    $('.log').on('click', function () {
+        alert('log');
+        sendToPlatform(['log'], function (message) {
+            $('.logger').prepend(message + '<br>');
+        });
+    });
+
+    document.addEventListener(
+        "backbutton",
+        function() { mosync.app.exit(); },
+        true
+    );
 })
