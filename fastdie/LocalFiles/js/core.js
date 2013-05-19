198e07b051ad4b271004f7d8f6fb3bac3bb96f12
Roman Sukhorukov <tas200@mail.ru>
2013-05-19 04:16:51 +0400

    Refactoring, new UI

--- fastdie/LocalFiles/js/core.js
+++ fastdie/LocalFiles/js/core.js
@@ -1,17 +1,184 @@
 // Register event listeners.
 
-// The "deviceready" event is sent when the system
-// has finished loading.
+// The "deviceready" event is fired when the system has finished loading.
 //document.addEventListener(
-    //"deviceready",
-    //initialize,
-    //true
+//    "deviceready",
+//    initialize,
+//    true
 //);
+document.addEventListener(
+	"deviceready",
+	handleEvents,
+	true
+);
 
-//document.addEventListener(
-    //'DOMContentLoaded',
-    //initialize
-//)
+document.addEventListener(
+	"DOMContentLoaded",
+	handleEvents
+);
+
+// addEventListener should get callback function instead of EventListener object
+function handleEvents(event) {
+	coreApp.handleEvent(event);
+}
+
+//var coreApp_Old = {
+//	get viewSwitcher() {
+//		return viewSwitcher;
+//	}
+//	init: function coreApp_init() {
+//	    // Close the application when the back key is pressed
+//	    document.addEventListener("backbutton", function() { mosync.app.exit(); }, true);
+//	    
+//	    try {
+//	    	this.viewSwitcher.init();
+//	    }
+//	    catch(e) {
+//	    	mosync.rlog("Couldn't init layout. "+ e);
+//	    	return;
+//	    }
+//	},
+//	handleEvent: function coreApp_handleEvent(event) {
+//		switch(event.type) {
+//		case "DOMContentLoaded":
+//			this.init();
+//			break;
+//		case "deviceready":
+//			break;
+//		}
+//	}
+//};
+
+var coreApp = function coreApp_constructor() {
+	var initialized = false;
+	var CONSTS = {
+		NAVIGATION_CLASS: "js-navigation"
+	}
+	
+	function coreApp__initialize() {
+	    // Close the application when the back key is pressed
+	    document.addEventListener("backbutton", function() { mosync.app.exit(); }, true);
+	    var viewSwitcher = getViewSwitcher();
+	    try {
+	    	viewSwitcher.init();
+	    }
+	    catch(e) {
+	    	mosync.rlog("Couldn't init layout. "+ e);
+	    	return;
+	    }
+	    
+	    $("."+ CONSTS.NAVIGATION_CLASS +">li").on("click", function() {
+	    	console.log("clicked; "+ $(this).attr("data-screen"));
+	    	viewSwitcher.show($(this).attr("data-screen"));
+	    });
+//	    var navigations = document.getElementsByClassName(CONSTS.NAVIGATION_CLASS);
+//	    var links = null;
+//	    for ( var navItem in navigations ) {
+//	    	$(navigations[navItem].querySelectorAll(">li")).click(function(){
+//	    		this
+//	    	});
+//	    }
+	    
+		initialized = true;
+	}
+	
+	function getViewSwitcher() {
+		return viewSwitcher;
+	}
+	
+	return {
+		init: function coreApp_init() {
+			if (initialized) {
+				return;
+			}
+			
+			coreApp__initialize.apply(this);
+		},
+		handleEvent: function coreApp_handleEvent(event) {
+			switch(event.type) {
+			case "DOMContentLoaded":
+				this.init();
+				break;
+			case "deviceready":
+				break;
+			}
+		}
+	}
+}();
+
+var viewSwitcher = function viewSwitcher_constructor() {
+	var initialized = false;
+	var viewPageArea = null;
+	var $overlay = null; // jQuery object
+	
+	var CONSTS = {
+		VIEW_AREA_ID: "view-page-area",
+		ACITVE_VIEW_CLASS: "active-view",
+		OVERLAY_ID: "view-switcher-overlay"
+	};
+	
+	function getViewName(viewTarget) {
+		return ".vp-"+ viewTarget;
+	}
+	
+	function viewSwitcher__initialize() {
+		viewPageArea = document.getElementById(CONSTS.VIEW_AREA_ID);
+		if (!viewPageArea) {
+			throw new Error("Broken layout");
+		}
+		
+		$overlay = $("<div id='"+ CONSTS.OVERLAY_ID +"'></div>");
+		$(document.body).append($overlay);
+		
+	    $( viewPageArea.querySelector(".vp-main") || ":first-child", viewPageArea).fadeIn(600, function(){ 
+	    	$(this).addClass(CONSTS.ACITVE_VIEW_CLASS); 
+	    	$overlay.hide(); 
+	    });
+	    
+		initialized = true;
+	}
+	
+	return {
+		init: function viewSwitcher_init() {
+			if (initialized) {
+				return;
+			}
+			
+			viewSwitcher__initialize.apply(this);
+		},
+		show: function viewSwitcher_show(view) {
+			var viewName = getViewName(view);
+			var $newView = null;
+			if (!($newView = $(viewName, viewPageArea)).length) {
+				return;
+			}
+			
+			$overlay.show();
+			
+			var curView = this.getCurrentView();
+			console.log( $(curView) );
+			$(curView).fadeOut(200, function() {
+				curView.classList.remove(CONSTS.ACITVE_VIEW_CLASS);
+				$newView.fadeIn(200, function() {
+					$newView.addClass(CONSTS.ACITVE_VIEW_CLASS);
+					$overlay.hide();
+				});
+			});
+		},
+		getCurrentView: function viewSwitcher_getCurrentView() {
+			return viewPageArea.querySelector("."+ CONSTS.ACITVE_VIEW_CLASS);
+		}
+	}
+}();
+
+var deviceUtils = {
+	vibrate: function deviceUtils_vibrate(duration) {
+		mosync.brindge.send(["Custom", "Vibrate", parseInt(duration, 0)]);
+	},
+	beep: function deviceUtils_beep() {
+		mosync.bridge.send(["Custom", "Beep"]);
+	}
+};
 
 //function changeColor()
 //{
@@ -33,46 +200,38 @@
 /**
  * Отправка сообщений бекенду
  *
- * @param message {String} message name
+ * @param message {Array} list of words
  * @param callback {Function}
  */
-var counter = 1;
-function sendToPlatform (message, callback) {
-    var arr = ['Custom', message, counter++];
+function sendToPlatform (messages, callback) {
+    var arr = ['Custom'],
+        $loading = $('.loading');
 
+    $loading.show();
+    arr = arr.concat(messages);
     mosync.bridge.send(
         arr,
-        callback
+        function (data) {
+            $loading.hide();
+            callback(data);
+        }
     );
 }
 
-/**
- * Генерация mustache шаблона
- *
- * @param templateName имя шаблона без префикса
- * @param data данные
- */
-function render (templateName, data) {
-    if (!templateName)
-        return '';
-
-    return Mustache.render($('.m-'+templateName).html(), data || null);
-}
-
-/**
-* Vibrate device.
-*/
-function vibrate()
-{
-    mosync.bridge.send(["Custom", "Vibrate", "500"]);
-}
-
-/**
-* Play one beep sound.
-*/
-function beep()
-{
-    // Send message to C++ to make device beep.
-    // Here we used the string stream format.
-    mosync.bridge.send(["Custom", "Beep"]);
-}
+///**
+//* Vibrate device.
+//*/
+//function vibrate()
+//{
+//    mosync.bridge.send(["Custom", "Vibrate", "500"]);
+//}
+//
+///**
+//* Play one beep sound.
+//*/
+//function beep()
+//{
+//    // Send message to C++ to make device beep.
+//    // Here we used the string stream format.
+//    mosync.bridge.send(["Custom", "Beep"]);
+//}
--- fastdie/LocalFiles/js/ui.js
+++ fastdie/LocalFiles/js/ui.js
@@ -1,36 +1,20 @@
-function onSuccess () {
-    $('body').html(JSON.stringify(arguments));
-}
-
-function onError (argument) {
-    $('body').html(JSON.stringify(arguments));
-}
-
-$(function () {
-    $('body').html('');
-    $('body').html('LOL');
-    $('body').prepend('1<br>');
-    setInterval(function() {
-        appCore.accelerometer.isDownPosition() && $('body').prepend('down<br>');
-        appCore.accelerometer.isBangPosition() && $('body').prepend('bang<br>');
-        appCore.accelerometer.isBadPosition() && $('body').prepend('bad<br>');
-    }, 50);
-    $('.find-device').on('click', function () {
-        $('.main-page').show();
-        $('.devices').show();
-        sendToPlatform('findDevices', function () {
-            $('.devices').prepend(JSON.stringify(arguments) + '<br>');
-        });
-    });
-    $('.log').on('click', function () {
-        sendToPlatform('log', function (message) {
-            $('.logger').append(message + '<br>');
-        });
-    });
-
-    document.addEventListener(
-        "backbutton",
-        function() { mosync.app.exit(); },
-        true
-    );
-});
+//function initialize () {
+//    // Close the application when the back key is pressed
+//    document.addEventListener(
+//        "backbutton",
+//        function() { mosync.app.exit(); },
+//        true
+//    );
+//
+//
+//}
+//$(function () {
+//    $('.find-device').on('click', function () {
+//        $('.main-page').hide();
+//        $('.devices').show();
+//        sendToPlatform(['findDevices'], function () {
+//            $('body').html(JSON.stringify(arguments));
+//            alert(JSON.stringify(arguments));
+//        })
+//    });
+//})
